192edb5229723043bc82e7657bfe2122f9fdd3cb
jeffkub <jeffkub@gmail.com>
2019-05-24 13:28:58 -0400

    Working on control system simulation

--- simulation/inverted-pendulum.m
+++ simulation/inverted-pendulum.m
@@ -1,20 +1,92 @@
+# inverted-pendulum.m
+#
+# Control system simulation for inverted pendulum. Based on Example 12-5 from
+# Modern Control Engineering, Fourth Edition by K. Ogata.
+
 pkg load control
 
-g = 9.81;   # m/s
-M = 2;      # kg
-m = 0.1;    # kg
-l = 0.5;    # m
+clear all
+close all
 
-A = [ 0             1 0 0;
-      (M+m)/(M*l)*g 0 0 0;
-      0             0 0 1;
-      -m/M*g        0 0 0];
-B = [ 0; -1/(M*l); 0; 1/M];
-C = [0 0 1 0];
+# Inverted pendulum parameters
+g = 9.81;   # Gravity constant [m/s]
+M = 2;      # Cart mass [kg]
+m = 0.1;    # Pendulum mass [kg]
+l = 0.5;    # Pendulum length to center of mass [m]
 
-Ahat = [A zeros(4,1); -C 0];
-Bhat = [B; 0];
-
+# Desired closed loop poles
 J = [-1+j*sqrt(3) -1-j*sqrt(3) -5 -5 -5];
 
+# State space representation of system
+A = [             0 1 0 0;
+      (M+m)/(M*l)*g 0 0 0;
+                  0 0 0 1;
+             -m/M*g 0 0 0];
+
+B = [0 -1/(M*l) 0 1/M]';
+
+C = [0 0 1 0];
+
+D = [0];
+
+Ahat = [ A zeros(4,1);
+        -C          0];
+
+Bhat = [B; 0];
+
+# Determine the state feedback gain matrix
 Khat = place(Ahat, Bhat, J);
+K = Khat(1:4)
+KI = -Khat(5)
+
+# Calculate the step response
+ts = 0.02;
+t = 0:ts:6;
+
+AA = [(A - B*K) B*KI;
+             -C    0];
+
+BB = [0 0 0 0 1]';
+
+CC = [C 0];
+
+DD = [0];
+
+sys = ss(AA, BB, CC, DD);
+[y, t, x] = step(sys, t);
+
+# Plot the step response
+subplot(3,2,1)
+plot(t, x(:,1))
+grid
+title('\theta vs t')
+xlabel('t [s]')
+ylabel('\theta [rad]')
+
+subplot(3,2,2)
+plot(t, x(:,2))
+grid
+title('\theta dot vs t')
+xlabel('t [s]')
+ylabel('\theta dot [rad/s]')
+
+subplot(3,2,3)
+plot(t, x(:,3))
+grid
+title('x vs t')
+xlabel('t [s]')
+ylabel('x [m]')
+
+subplot(3,2,4)
+plot(t, x(:,4))
+grid
+title('x dot vs t')
+xlabel('t [s]')
+ylabel('x dot [m/s]')
+
+subplot(3,2,5)
+plot(t, x(:,5))
+grid
+title('\zeta vs t')
+xlabel('t [s]')
+ylabel('\zeta [m]')
