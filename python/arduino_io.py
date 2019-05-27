4761ed73e7d25d7e5eccf77afe72e1a477cca125
Jeff Kubascik <jeffkub@gmail.com>
2019-05-27 08:30:51 -0700

    Split up parameters

--- python/arduino_io.py
+++ python/arduino_io.py
@@ -2,38 +2,30 @@ import serial
 
 class ArduinoIO:
 	def __init__(self, params, port, baud=115200):
-		self.x_scale = params['x_scale']['val']
-		self.theta_scale = params['theta_scale']['val']
-		self.motor_scale = params['motor_scale']['val']
-		self.motor_offset = params['motor_offset']['val']
+		self.encoder_scale = params['encoder_scale']['val']
+		self.motor_v_scale = params['motor_v_scale']['val']
 
-		self.x = 0.0
-		self.x_offset = 0.0
+		self.encoder = [0.0] * 2
+		self.encoder_offset = [0.0] * 2
 
-		self.theta = 0.0
-		self.theta_offset = 0.0
-
-		self.switches = [0] * 2
+		self.switch = [False] * 2
 
 		self.serial = serial.Serial(port, baud, timeout=0.2)
 
 	def __del__(self):
 		self.serial.close()
 
-	def getX(self):
-		return self.x + self.x_offset
+	def getEncoder(self, index):
+		# Unit is [revs]
+		return self.encoder[index] + self.encoder_offset[index]
 
-	def setXOffset(self, offset):
-		self.x_offset += offset
-
-	def getTheta(self):
-		return self.theta + self.theta_offset
-
-	def setThetaOffset(self, offset):
-		self.theta_offset += offset
+	def setEncoderOffset(self, index, offset):
+		# Unit is [revs]
+		self.encoder_offset[index] += offset
 
 	def getSwitch(self, index):
-		return self.switches[index]
+		# True if switch is activated, else False
+		return self.switch[index]
 
 	def read(self):
 		self.serial.reset_input_buffer()
@@ -48,19 +40,15 @@ class ArduinoIO:
 			print('Invalid response')
 			return
 
-		self.x = float(vals[0]) * self.x_scale
-		self.theta = float(vals[1]) * self.theta_scale
+		self.encoder[0] = float(vals[0]) / self.encoder_scale
+		self.encoder[1] = float(vals[1]) / self.encoder_scale
 
-		self.switches[0] = int(vals[2]) > 0
-		self.switches[1] = int(vals[3]) > 0
+		self.switch[0] = int(vals[2]) > 0
+		self.switch[1] = int(vals[3]) > 0
 
-	def setMotor(self, force):
-		if force > 0:
-			force += self.motor_offset
-		elif force < 0:
-			force -= self.motor_offset
-
-		val = int(force / self.motor_scale)
+	def setMotorV(self, voltage):
+		# Unit is [V]
+		val = int(voltage / self.motor_v_scale)
 		clamped = max(-2047, min(2047, val))
 
 		msg = 'M1: ' + str(clamped) + '\n'
--- python/cart.py
+++ python/cart.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 
 class Cart:
@@ -5,64 +7,80 @@ class Cart:
 		self.sensors = sensors
 		self.motor = motor
 
+		pulley_teeth = params['pulley_teeth']['val']
+		belt_pitch = params['belt_pitch']['val']
+
 		self.guard = params['guard']['val']
+
+		self.x_scale = belt_pitch * pulley_teeth	# [m/rev]
+		self.theta_scale = 2 * math.pi				# [rad/rev]
 
 		self.limit = 0.0
 
 		self.last_theta = 0.0
 		self.last_x = 0.0
 
+	def _getX(self):
+		# Unit is [m]
+		return self.sensors.getEncoder(0) * self.x_scale
+
+	def _getTheta(self):
+		# Unit is [rad]
+		return self.sensors.getEncoder(1) * self.theta_scale
+
 	def findLimits(self):
 		self.sensors.read()
 
-		self.motor.setMotor(-0.4)
+		self.motor.setMotorV(-5)
 		while not self.sensors.getSwitch(0):
 			self.sensors.read()
 
-		x_min = self.sensors.getX()
+		x_min = self.sensors.getEncoder(0)
 
-		self.motor.setMotor(0.4)
+		self.motor.setMotorV(5)
 		while not self.sensors.getSwitch(1):
 			self.sensors.read()
 
-		x_max = self.sensors.getX()
+		x_max = self.sensors.getEncoder(0)
 
-		self.motor.setMotor(0.0)
-
-		self.limit = (x_max - x_min) / 2
+		self.motor.setMotorV(0)
 
 		offset = (x_min + x_max) / 2
-		self.sensors.setXOffset(-offset)
+		self.sensors.setEncoderOffset(0, -offset)
+
+		self.limit = (x_max - x_min) / 2 * self.x_scale
 
 	def zeroTheta(self):
 		self.sensors.read()
 
-		offset = self.sensors.getTheta()
-		self.sensors.setThetaOffset(-offset)
+		offset = self.sensors.getEncoder(1)
+		self.sensors.setEncoderOffset(1, -offset)
 
 	def goTo(self, position):
+		# Unit is [m]
 		self.sensors.read()
 
-		if (self.sensors.getX() - position) < -0.01:
-			self.motor.setMotor(0.4)
-			while (self.sensors.getX() - position) < -0.01:
+		if (self._getX() - position) < -0.01:
+			self.motor.setMotorV(5)
+			while (self._getX() - position) < -0.01:
 				self.sensors.read()
 
-		elif (self.sensors.getX() - position) > 0.01:
-			self.motor.setMotor(-0.4)
-			while (self.sensors.getX() - position) > 0.01:
+		elif (self._getX() - position) > 0.01:
+			self.motor.setMotorV(-5)
+			while (self._getX() - position) > 0.01:
 				self.sensors.read()
 
-		self.motor.setMotor(0.0)
+		self.motor.setMotorV(0)
 
 	def checkLimits(self):
 		return abs(self.last_x) < (self.limit - self.guard)
 
 	def nextState(self, dt):
+		# Unit is [s]
 		self.sensors.read()
 
-		x = self.sensors.getX()
-		theta = self.sensors.getTheta()
+		x = self._getX()
+		theta = self._getTheta()
 
 		# State vector is [theta, theta_dot, x, x_dot]
 		state = np.array([
@@ -78,4 +96,6 @@ class Cart:
 		return state, x
 
 	def setForce(self, force):
-		self.motor.setMotor(force)
+		# Unit is [N]
+		# TODO: Implement motor equation
+		self.motor.setMotorV(force)
